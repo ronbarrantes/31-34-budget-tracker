40f02a381d4990ad84053d37d90d64d55a329827
Ron Barrantes <ronbarrantes@gmail.com>
2017-10-24 14:22:31 -0700

    added reducer

--- lab-ron/src/main.js
+++ lab-ron/src/main.js
@@ -1,1 +1,26 @@
-console.log('hello')
+import './style/main.scss'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './component/app'
+import categoriesReducer from './reducer/categories.js'
+
+let store = createStore(categoriesReducer)
+
+store.subscribe(()=> {
+  console.log('STATE ::', store.getState())
+})
+
+const container = document.createElement('div')
+document.body.appendChild(container)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  container)
+
+
